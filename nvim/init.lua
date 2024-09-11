c8d46b60101e7aaf23961a31b5c8fe242b85af79
Marcel Hofmann <marcel.hofmann@deepimmo.com>
2024-09-11 08:41:41 +0200

    feat: prettier for typescript files

--- nvim/init.lua
+++ nvim/init.lua
@@ -443,16 +443,14 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
-          enabled = false,
+
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectories = { mode = 'auto' },
+          },
         },
+
         vtsls = {
           -- explicitly add default filetypes, so that we can extend
           -- them in related extras
@@ -604,7 +602,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { 'prettierd' },
+        javascript = { 'prettierd', 'eslint', 'prettier', stop_after_first = true },
       },
     },
   },
